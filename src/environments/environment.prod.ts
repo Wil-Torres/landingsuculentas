46dde2687ed8fd47ce0d1a49d38a48cc29293cec
Wtorres <wtorres@cads.com.gt>
2018-08-16 17:20:35 -0600

    creacion de proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC_SxUejgh4t9M1YLoy661wr3USOnksVc8",
+    authDomain: "landingsuculentasgrunegt.firebaseapp.com",
+    databaseURL: "https://landingsuculentasgrunegt.firebaseio.com",
+    projectId: "landingsuculentasgrunegt",
+    storageBucket: "landingsuculentasgrunegt.appspot.com",
+    messagingSenderId: "134104384537"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC_SxUejgh4t9M1YLoy661wr3USOnksVc8",
+    authDomain: "landingsuculentasgrunegt.firebaseapp.com",
+    databaseURL: "https://landingsuculentasgrunegt.firebaseio.com",
+    projectId: "landingsuculentasgrunegt",
+    storageBucket: "landingsuculentasgrunegt.appspot.com",
+    messagingSenderId: "134104384537"
+  }
 };
 
 /*
