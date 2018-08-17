d624b5ded74f772b10a5419ef2e5cb9bc515d28b
Wtorres <wtorres@cads.com.gt>
2018-08-17 16:19:13 -0600

    se agrega funcionalidad al envio de correo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LandingService } from './services/landing.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  objeto: any = {};
   title = 'app';
+  constructor(private serviceLanding: LandingService) { }
+  guardar() {
+    if (this.objeto.length !== 0) {
+      this.objeto.fecha = new Date();
+      this.serviceLanding.addContacto(this.objeto).then((res) => {
+        this.objeto = {};
+        window.alert('¡Gracias por escribirnos! Tu solicitud ha sido enviada exitosamente y pronto un asesor se pondrá en contacto')
+      }, err => {
+        window.alert(err);
+      });
+    }
+  }
 }
